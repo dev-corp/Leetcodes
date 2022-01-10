3391fbbbc64784db537a2664985f45a77023b658
Dev Garg <70332477+dev-corp@users.noreply.github.com>
2022-01-10 19:31:54 +0530

    Time: 1 ms (64.31%), Space: 40.2 MB (11.12%) - LeetHub

---
+++ 78-subsets/78-subsets.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums)
+    {
+           List<List<Integer>> list=new ArrayList<List<Integer>>();
+        
+        
+     BackTrack(list,nums,new ArrayList<>(),0);
+        
+        return list;
+    }
+        public void BackTrack(List<List<Integer>> list, int[] nums, List<Integer> arr, int index)
+        {
+            list.add(new ArrayList<>(arr));
+
+            for(int i = index; i < nums.length; i++){
+            arr.add(nums[i]);
+            BackTrack(list, nums, arr, i + 1);
+            arr.remove(arr.size() - 1);
+
+        }
+    }
+}
