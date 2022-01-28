d422d207a9d5651ad5fc3b5debba00f5df35996d
Dev Garg <70332477+dev-corp@users.noreply.github.com>
2022-01-28 22:00:29 +0530

    Time: 1 ms (51.33%), Space: 43.8 MB (5.14%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -15,29 +15,41 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        List<Integer> answer = new ArrayList<>();
-        inorderTraversal(root,answer);
-        
-        boolean isBST = true;
-        for(int i = 1; i<answer.size(); i++){
-            isBST = isBST && answer.get(i-1)<answer.get(i) ? true : false;
-        }
-        
-        return isBST;
+        return checkBst(root,Long.MIN_VALUE,Long.MAX_VALUE);
     }
+    public boolean checkBst(TreeNode root, long min, long max){
+        if(root == null)
+            return true;
+        if(root.val <= min || root.val >= max)
+            return false;
+        return  checkBst(root.left,min,root.val) && 
+                checkBst(root.right,root.val,max);
+    }
+
+//     public boolean isValidBST(TreeNode root) {
+//         List<Integer> answer = new ArrayList<>();
+//         inorderTraversal(root,answer);
+        
+//         boolean isBST = true;
+//         for(int i = 1; i<answer.size(); i++){
+//             isBST = isBST && answer.get(i-1)<answer.get(i) ? true : false;
+//         }
+        
+//         return isBST;
+//     }
     
-    private void inorderTraversal(TreeNode root, List<Integer> answer){
+//     private void inorderTraversal(TreeNode root, List<Integer> answer){
         
-        if(root==null){
-            return;
-        }
+//         if(root==null){
+//             return;
+//         }
         
-        inorderTraversal(root.left,answer);
-        answer.add(root.val);
-        inorderTraversal(root.right,answer);
+//         inorderTraversal(root.left,answer);
+//         answer.add(root.val);
+//         inorderTraversal(root.right,answer);
         
-        return;
+//         return;
         
-    }
+//     }
 
 }
