cd66d72a40e370409cd65c5752767823f2c40ffd
Dev Garg <70332477+dev-corp@users.noreply.github.com>
2022-04-29 23:31:37 +0530

    Time: 1 ms (45.53%), Space: 41.9 MB (7.10%) - LeetHub

---
+++ 476-number-complement/476-number-complement.java
@@ -0,0 +1,21 @@
+class Solution 
+{
+    public int findComplement(int num)
+    {
+        String binaryString=Integer.toBinaryString(num);
+        StringBuilder str=new StringBuilder();
+        for(int i=0;i<binaryString.length();i++)
+        {
+            if(binaryString.charAt(i)=='0')
+            {
+                str.append('1');
+            }
+            else
+            {
+                str.append('0');
+            }
+        }
+        
+        return Integer.parseInt(str.toString(),2);
+    }
+}
