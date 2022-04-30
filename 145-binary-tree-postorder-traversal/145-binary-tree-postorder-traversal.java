26994f8752633e353fc44b7c003406dd4c1b9c96
Dev Garg <70332477+dev-corp@users.noreply.github.com>
2022-04-30 12:15:53 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (53.57%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -13,25 +13,43 @@
  *     }
  * }
  */
-class Solution {
-    public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> answer = new ArrayList<>();
-        
-        postorder(root, answer);
-        
-        return answer;
-    }
-    
-    private void postorder(TreeNode root, List<Integer> answer)
-    {
-        if(root == null)
-        {
-            return;
-        }
 
-        postorder(root.left, answer);
-        postorder(root.right, answer);
-        answer.add(root.val);
-        return;
+public class Solution 
+{
+    public List<Integer> postorderTraversal(TreeNode root) 
+    {
+        List<Integer> result = new ArrayList<Integer>();
+        if (root != null)
+        {
+            result.addAll(postorderTraversal(root.left));
+            result.addAll(postorderTraversal(root.right));
+            result.add(root.val);
+        }
+        return result;
     }
 }
+
+
+
+// class Solution {
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         List<Integer> answer = new ArrayList<>();
+        
+//         postorder(root, answer);
+        
+//         return answer;
+//     }
+    
+//     private void postorder(TreeNode root, List<Integer> answer)
+//     {
+//         if(root == null)
+//         {
+//             return;
+//         }
+
+//         postorder(root.left, answer);
+//         postorder(root.right, answer);
+//         answer.add(root.val);
+//         return;
+//     }
+// }
