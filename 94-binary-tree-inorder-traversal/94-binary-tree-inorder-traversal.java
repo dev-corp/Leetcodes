4d70da61add5410af0f343f5e647692f41ae03f7
Dev Garg <70332477+dev-corp@users.noreply.github.com>
2022-04-30 11:58:10 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (39.16%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -13,25 +13,41 @@
  *     }
  * }
  */
-class Solution {
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> answer = new ArrayList<>();
-        
-        inorder(root, answer);
-        
-        return answer;
-    }
-    
-    private void inorder(TreeNode root, List<Integer> answer)
-    {
-        if(root == null)
-        {
-            return;
-        }
 
-        inorder(root.left, answer);
-        answer.add(root.val);
-        inorder(root.right, answer);
-        return;
+public class Solution 
+{
+    public List<Integer> inorderTraversal(TreeNode root) 
+    {
+        List<Integer> result = new ArrayList<Integer>();
+        if (root != null)
+        {
+            result.addAll(inorderTraversal(root.left));
+            result.add(root.val);
+            result.addAll(inorderTraversal(root.right));
+        }
+        return result;
     }
 }
+
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> answer = new ArrayList<>();
+        
+//         inorder(root, answer);
+        
+//         return answer;
+//     }
+    
+//     private void inorder(TreeNode root, List<Integer> answer)
+//     {
+//         if(root == null)
+//         {
+//             return;
+//         }
+
+//         inorder(root.left, answer);
+//         answer.add(root.val);
+//         inorder(root.right, answer);
+//         return;
+//     }
+// }
