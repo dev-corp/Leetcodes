3f6a6c10161d2bae15d2035fd6495d8e5981aaea
Dev Garg <70332477+dev-corp@users.noreply.github.com>
2022-04-30 18:06:21 +0530

    Time: 1 ms (81.65%), Space: 43.9 MB (18.69%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
@@ -13,44 +13,82 @@
  *     }
  * }
  */
-class Solution {
-    public List<List<Integer>> levelOrder(TreeNode root)
+public class Solution 
+{
+    public List<List<Integer>> levelOrder(TreeNode root) 
     {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
         
-        List<List<Integer>> answer = new ArrayList<>();
-    
-        Queue<TreeNode> queue = new LinkedList<>();
+        if(root == null) 
+            return wrapList;
         
-        if(root == null)
-            return answer;
-        
-        queue.add(root);
-    
+        queue.offer(root);
         while(!queue.isEmpty())
         {
-            
-            int currentSize = queue.size();
-            List<Integer> currentAns = new ArrayList<>();
-            
-            while(currentSize > 0)
+            int levelNum = queue.size();
+            List<Integer> subList = new LinkedList<Integer>();
+            for(int i=0; i<levelNum; i++) 
             {
-                TreeNode currentNode = queue.remove();
-                currentAns.add(currentNode.val);
-            
-                if(currentNode.left != null)
-                    queue.add(currentNode.left);
-            
-                if(currentNode.right != null){
-                    queue.add(currentNode.right);
+                if(queue.peek().left != null) queue.offer(queue.peek().left);
+                if(queue.peek().right != null) queue.offer(queue.peek().right);
+                subList.add(queue.poll().val);
             }
-                currentSize--;
+            wrapList.add(subList);
         }
-            
-        answer.add(currentAns);
-            
-    }
-        
-    return answer;
-        
+        return wrapList;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public List<List<Integer>> levelOrder(TreeNode root)
+//     {
+        
+//         List<List<Integer>> answer = new ArrayList<>();
+    
+//         Queue<TreeNode> queue = new LinkedList<>();
+        
+//         if(root == null)
+//             return answer;
+        
+//         queue.add(root);
+    
+//         while(!queue.isEmpty())
+//         {
+            
+//             int currentSize = queue.size();
+//             List<Integer> currentAns = new ArrayList<>();
+            
+//             while(currentSize > 0)
+//             {
+//                 TreeNode currentNode = queue.remove();
+//                 currentAns.add(currentNode.val);
+            
+//                 if(currentNode.left != null)
+//                     queue.add(currentNode.left);
+            
+//                 if(currentNode.right != null){
+//                     queue.add(currentNode.right);
+//             }
+//                 currentSize--;
+//         }
+            
+//         answer.add(currentAns);
+            
+//     }
+        
+//     return answer;
+        
+//     }
+// }
