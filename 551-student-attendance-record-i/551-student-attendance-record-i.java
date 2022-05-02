46ed5b2d8d29e8849e6913986c123ad5e8539ea9
Dev Garg <70332477+dev-corp@users.noreply.github.com>
2022-05-02 21:51:48 +0530

    Time: 1 ms (63.03%), Space: 42.2 MB (43.13%) - LeetHub

--- 551-student-attendance-record-i/551-student-attendance-record-i.java
+++ 551-student-attendance-record-i/551-student-attendance-record-i.java
@@ -1,37 +1,43 @@
-class Solution {
-    public boolean checkRecord(String s) 
-    {
-    		if (s.contains("LLL")) 
-                return false;
-    		
-    		String str = s.replaceAll("A", "");
-    		
-    		if ((s.length() - str.length()) > 1)
-    		{
-    			return false;
-    		}
-    		
-    		
-        return true;
-        
-    }
-}
 // class Solution {
-//     public boolean checkRecord(String s) {
-//         for (int i=0;i<s.length();i++)
-//         {
-//             int tot_absent = 0, ;
-//             if(s.charAt(i) == 'P')
-//                 continue 
-                
-//             if(s.charAt(i) == 'A')
-//             {
-//                 tot_absent++;
-//                 if(tot_absent >2)
-//                     return false;
-//             }
-            
-//             if(s.c)
-//         }
+//     public boolean checkRecord(String s) 
+//     {
+//     		if (s.contains("LLL")) 
+//                 return false;
+    		
+//     		String str = s.replaceAll("A", "");
+    		
+//     		if ((s.length() - str.length()) > 1)
+//     		{
+//     			return false;
+//     		}
+    		
+    		
+//         return true;
+        
 //     }
 // }
+class Solution 
+{
+    public boolean checkRecord(String s) 
+    {
+        int countA=0;
+        int continuosL = 0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i) == 'A')
+            {
+                countA++;
+                continuosL = 0;
+            }
+            else if(s.charAt(i) == 'L')
+                continuosL++;
+            else
+                continuosL = 0;
+            
+            if(countA >1 || continuosL > 2 )
+                return false;
+        }
+        return true;
+
+    }
+}
