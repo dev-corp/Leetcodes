1a75f659cf3508942c87e945439781636c249cac
Dev Garg <70332477+dev-corp@users.noreply.github.com>
2022-05-06 23:45:39 +0530

    Time: 79 ms (36.07%), Space: 49.6 MB (54.38%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -0,0 +1,45 @@
+class Solution 
+{
+/*  1. add pair(char, count) to stack
+    2. pop pair when count==k
+*/
+public String removeDuplicates(String s, int k)
+{
+    int len=s.length();
+    Stack<Node> stack = new Stack<>();
+    for(char c : s.toCharArray()){
+        //compare prev with cur
+        if(!stack.isEmpty() && stack.peek().c == c)
+        {
+            stack.peek().count++;
+        }else
+        {
+            //not repeat
+            stack.push(new Node(c,1));
+        }
+        //remove when num of dup == k
+        if(stack.peek().count==k) stack.pop();
+    }
+    //build result
+    StringBuilder sb = new StringBuilder();
+    for(Node node : stack)
+    {
+        for(int i=0;i<node.count;i++)
+        {
+            sb.append(node.c);
+        }
+    }
+    return sb.toString();
+}
+
+class Node
+{
+    char c;
+    int count;
+    public Node(char c, int count)
+    {
+        this.c=c;
+        this.count=count;
+    }
+}
+}
