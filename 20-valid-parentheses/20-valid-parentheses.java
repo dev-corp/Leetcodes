b538df2d1420ab94810be147d9a356236606ebd3
Dev Garg <70332477+dev-corp@users.noreply.github.com>
2022-06-07 14:22:05 +0530

    Time: 4 ms (23.02%), Space: 41.8 MB (63.89%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -17,3 +17,6 @@ class Solution // https://www.youtube.com/watch?v=3gqYc56p9qU
 	return stack.isEmpty();
 }
 }
+/*
+The basic idea is to push the right parentheses ')', ']', or '}' into the stack each time when we encounter left ones. And if a right bracket appears in the string, we need check if the stack is empty and also whether the top element is the same with that right bracket. If not, the string is not a valid one. At last, we also need check if the stack is empty.
+*/
