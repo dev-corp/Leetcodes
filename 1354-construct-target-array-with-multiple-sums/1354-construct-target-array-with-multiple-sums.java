7b8544ad8e0789d54b2fad33dd2ad06fe1f45cc0
Dev Garg <70332477+dev-corp@users.noreply.github.com>
2022-06-27 00:23:44 +0530

    Time: 9 ms (88.94%), Space: 49.7 MB (94.06%) - LeetHub

---
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.java
@@ -0,0 +1,28 @@
+class Solution // https://leetcode.com/problems/construct-target-array-with-multiple-sums/discuss/2189445/Visual-Explanation-or-JAVA-Max-Heap
+{
+    public boolean isPossible(int[] target)
+    {
+	if (target.length == 1) return target[0] == 1;
+
+	PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+	int sum = 0;
+	for (int t: target)
+    {
+		pq.add(t);
+		sum += t;
+	}
+
+	while (pq.peek() != 1) 
+    {
+		int curr = pq.poll();
+		if (sum - curr == 1) return true;
+
+		int x = curr % (sum - curr);
+		sum = sum - curr + x;
+
+		if (x == 0 || x == curr) return false;
+		else pq.add(x);
+	}
+	return true;
+    }
+}
