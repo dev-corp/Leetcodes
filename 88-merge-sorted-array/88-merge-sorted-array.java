02a6bf5a1fad7e35d393c72711de5b949f752cad
Dev Garg <70332477+dev-corp@users.noreply.github.com>
2022-07-06 23:08:02 +0530

    Time: 1 ms (35.69%), Space: 43.3 MB (9.51%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -8,3 +8,24 @@ class Solution {
     Arrays.sort(nums1);
     }
 }
+
+// class Solution 
+// {
+//     public void merge(int[] nums1, int m, int[] nums2, int n)
+//     {
+//     int finished = m+n-1;
+//     int tail1 = m-1;
+//     int tail2 = n-1;
+    
+//     while(tail1>=0 && tail2>=0){
+//         if(nums1[tail1]> nums2[tail2]){
+//             nums1[finished--] = nums1[tail1--];
+//         }
+//         else
+//          nums1[finished--] = nums2[tail2--];   
+//     }
+    
+//     while(tail2 >=0)
+//         nums1[finished--] = nums2[tail2--];
+//     }
+// }
