410c2e52fa82b9533cf6843b1a29ec53e4a7f08e
Dev Garg <70332477+dev-corp@users.noreply.github.com>
2022-07-11 00:26:23 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (81.18%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -14,14 +14,14 @@ class Solution
      {
          //1 -> Make Dummy node null and connect to the 1 node of given list.
          //2 -> Point head to the next connecting node
-        ListNode newHead = null;
+        ListNode newHead = null;//Initilize dummy node to null
         
         while (head != null) 
         {
-            ListNode next = head.next;
-            head.next = newHead;
-            newHead = head;
-            head = next;
+            ListNode next = head.next;// we create new next and pointed to imidiate of next
+            head.next = newHead;// we broke the hext pointer of head and pointed to dummy node
+            newHead = head;// moved dummy node to head
+            head = next;//move head to next head
         }
         return newHead;
     }
