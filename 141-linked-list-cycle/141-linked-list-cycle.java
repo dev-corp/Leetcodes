8aaf6176979b4d4dd963b366dcde3055a8c318de
Dev Garg <70332477+dev-corp@users.noreply.github.com>
2022-07-11 22:27:38 +0530

    Time: 1 ms (44.50%), Space: 45.5 MB (70.62%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -9,30 +9,24 @@
  *     }
  * }
  */
-public class Solution {
+public class Solution
+{
     public boolean hasCycle(ListNode head)
     {
         if(head==null)
-        {
             return false;
-        }
-        
-        ListNode slowPtr = head;
-        
-        ListNode fastPtr = head;
-        
-        while(fastPtr.next != null && fastPtr.next.next != null){
-            
+                                // We will take 2 pointer 
+        ListNode slowPtr = head;// Slow pointer iterate at 1 step
+        ListNode fastPtr = head;// Fast pointer iterate at 2 step
+                            // There is a certainty that there fast and slow pointer gonna meet if there is a cycle
+        while(fastPtr.next != null && fastPtr.next.next != null)
+        {    
             slowPtr = slowPtr.next;
             fastPtr = fastPtr.next.next;
             
             if(slowPtr == fastPtr)
-            {
                 return true;
-            }
         }
-        
         return false;
-        
     }
 }
