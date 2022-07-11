20ab1bcf33681d8caa1b5ba9a70e0aa80c0a32ae
Dev Garg <70332477+dev-corp@users.noreply.github.com>
2022-07-11 09:40:05 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (95.02%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
@@ -12,12 +12,12 @@ class Solution
 {
     public ListNode middleNode(ListNode head)
     {
-        ListNode fastPtr = head;
+        ListNode fastPtr = head;//at starting fast pointer and slow pointer at the head
         ListNode slowPtr = head;
         while(fastPtr.next!=null && fastPtr.next.next!=null)
-        {
-            fastPtr = fastPtr.next.next;
-            slowPtr = slowPtr.next;
+        {                                                   
+            fastPtr = fastPtr.next.next;//while fast pointer increments by 2
+            slowPtr = slowPtr.next;//but at each iteration slow pointer increments at 1
         }
         if(fastPtr.next!=null)
         {
