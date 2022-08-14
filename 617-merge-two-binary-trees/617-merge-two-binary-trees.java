5308c30264626d78d74d58407d915246437c41bf
Dev Garg <70332477+dev-corp@users.noreply.github.com>
2022-08-14 19:07:18 +0530

    Time: 1 ms (81.09%), Space: 51 MB (43.99%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.java
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.java
@@ -13,12 +13,20 @@
  *     }
  * }
  */
-class Solution {
-    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
-        if(root1==null){
+class Solution 
+{
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) 
+    {
+        if(root1 == null && root2 ==  null)
+        {
+            return null;
+        }
+        if(root1==null)
+        {
             return root2;
         }
-        if(root2==null){
+        if(root2==null)
+        {
             return root1;
         }
         
