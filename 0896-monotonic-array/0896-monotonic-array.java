a0bc1626f6e3c61a7968a89345947baff4501b85
Dev Garg <70332477+dev-corp@users.noreply.github.com>
2023-09-29 23:00:09 +0530

    Time: 1 ms (99.82%), Space: 55.3 MB (73.99%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -1,11 +1,16 @@
-class Solution {
-    public boolean isMonotonic(int[] A) {
-     boolean increasing = false, decreasing = false;
-     for(int i = 1; i< A.length; i++){
-         if(increasing && decreasing) return false;
-         if(A[i] < A[i-1]) decreasing = true;
-         if(A[i] > A[i-1]) increasing = true;
-     }
-     return increasing && decreasing ? false: true;
+class Solution 
+{
+    public boolean isMonotonic(int[] A) 
+    {
+         boolean increasing = false, decreasing = false;
+         for(int i = 1; i< A.length; i++){
+             if(increasing && decreasing) 
+                 return false;
+             if(A[i] < A[i-1])
+                 decreasing = true;
+             if(A[i] > A[i-1])
+                 increasing = true;
+         }
+         return increasing && decreasing ? false: true;
     }
 }
