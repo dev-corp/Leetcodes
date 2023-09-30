0429f86a462f3a460b806f423bb261749a81ce6c
Dev Garg <70332477+dev-corp@users.noreply.github.com>
2023-09-30 14:35:02 +0530

    Time: 23 ms (65.21%), Space: 63.2 MB (52.15%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.java
+++ 0456-132-pattern/0456-132-pattern.java
@@ -7,7 +7,7 @@ class Solution {
                 //secondMax = Math.max(stack.pop(), secondMax);  // not required because we are pushing only the number >secondMax
                 secondMax = stack.pop(); // because max will be nums[i] now.
             }
-            System.out.println(nums[i]+".."+ secondMax);
+            // System.out.println(nums[i]+".."+ secondMax);
             if (nums[i] > secondMax) stack.push(nums[i]); // this can be candidate for secondMax
             if (nums[i] < secondMax) return true; // as we found number less than second Max.
         }
