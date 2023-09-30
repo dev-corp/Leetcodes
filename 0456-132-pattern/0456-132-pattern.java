2ce1b1adaf23feac23d9806a48d8994d1baaf634
Dev Garg <70332477+dev-corp@users.noreply.github.com>
2023-09-30 14:33:22 +0530

    Time: 539 ms (5.04%), Space: 63.5 MB (33.58%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.java
+++ 0456-132-pattern/0456-132-pattern.java
@@ -1,13 +1,15 @@
 class Solution {
     public boolean find132pattern(int[] nums) {
         Stack<Integer> stack = new Stack<>();
-        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
         for (int i = nums.length - 1; i >= 0; i--) {
             while (!stack.isEmpty() && stack.peek() < nums[i]) {
-                max = stack.pop();
+                //secondMax = Math.max(stack.pop(), secondMax);  // not required because we are pushing only the number >secondMax
+                secondMax = stack.pop(); // because max will be nums[i] now.
             }
-            if (nums[i] > max) stack.push(nums[i]);
-            if (nums[i] < max) return true;
+            System.out.println(nums[i]+".."+ secondMax);
+            if (nums[i] > secondMax) stack.push(nums[i]); // this can be candidate for secondMax
+            if (nums[i] < secondMax) return true; // as we found number less than second Max.
         }
         return false;
     }
