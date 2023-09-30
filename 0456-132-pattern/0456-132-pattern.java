cd5918725a70d12b9eb67f42a71431434e1f54de
Dev Garg <70332477+dev-corp@users.noreply.github.com>
2023-09-30 14:44:44 +0530

    Time: 24 ms (63.06%), Space: 62.8 MB (82.46%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.java
+++ 0456-132-pattern/0456-132-pattern.java
@@ -3,13 +3,15 @@ class Solution {
         Stack<Integer> stack = new Stack<>();
         int secondMax = Integer.MIN_VALUE;
         for (int i = nums.length - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && stack.peek() < nums[i]) {
-                //secondMax = Math.max(stack.pop(), secondMax);  // not required because we are pushing only the number >secondMax
-                secondMax = stack.pop(); // because max will be nums[i] now.
+            while (!stack.isEmpty() && stack.peek() < nums[i])
+            {
+                secondMax = stack.pop(); // because secondMax will be nums[i] now.
             }
             // System.out.println(nums[i]+".."+ secondMax);
-            if (nums[i] > secondMax) stack.push(nums[i]); // this can be candidate for secondMax
-            if (nums[i] < secondMax) return true; // as we found number less than second Max.
+            if (nums[i] > secondMax) 
+                stack.push(nums[i]); // this can be candidate for secondMax
+            if (nums[i] < secondMax) 
+                return true; // as we found number less than second Max.
         }
         return false;
     }
