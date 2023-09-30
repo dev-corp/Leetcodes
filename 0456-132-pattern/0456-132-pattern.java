db9f62465436ef80b24cabc0a2bb5dea1c1189b6
Dev Garg <70332477+dev-corp@users.noreply.github.com>
2023-09-30 14:24:31 +0530

    Time: 24 ms (63.06%), Space: 62.8 MB (82.46%) - LeetHub

---
+++ 0456-132-pattern/0456-132-pattern.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        Stack<Integer> stack = new Stack<>();
+        int max = Integer.MIN_VALUE;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() < nums[i]) {
+                max = stack.pop();
+            }
+            if (nums[i] > max) stack.push(nums[i]);
+            if (nums[i] < max) return true;
+        }
+        return false;
+    }
+}
