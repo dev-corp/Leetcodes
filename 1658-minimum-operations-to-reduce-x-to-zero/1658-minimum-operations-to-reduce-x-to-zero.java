3c4219a2ce2f478f778686b3ea6e602df43fdf59
Dev Garg <70332477+dev-corp@users.noreply.github.com>
2023-10-01 16:35:57 +0530

    Time: 5 ms (52.43%), Space: 58.7 MB (81.39%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -9,7 +9,7 @@ class Solution {
             while (l <= r && currSum > sum - x) currSum -= nums[l++];
             if (currSum == sum - x) maxLength = Math.max(maxLength, r-l+1);
         }
-
         return maxLength == -1 ? -1 : nums.length - maxLength;
     }
 }
+// https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/discuss/2136570/Change-Your-Perspective-or-JAVA-Explanation/
