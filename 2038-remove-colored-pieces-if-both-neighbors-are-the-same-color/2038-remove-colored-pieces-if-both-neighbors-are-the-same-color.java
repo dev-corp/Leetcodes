901f0bee1585cdce1583627c576fb0ab4bc9c5c5
Dev Garg <70332477+dev-corp@users.noreply.github.com>
2023-10-02 20:04:08 +0530

    Time: 16 ms (52.37%), Space: 44 MB (81.16%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -7,7 +7,6 @@ class Solution {
             if (colors.charAt(i-1) == 'A' && colors.charAt(i) == 'A' && colors.charAt(i+1) == 'A') a++;
             if (colors.charAt(i-1) == 'B' && colors.charAt(i) == 'B' && colors.charAt(i+1) == 'B') b++;
         }
-
         return a > b;
     }
 }
